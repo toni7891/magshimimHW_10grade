125c017e7e188dbdcead4d67a0d9ebe5cc965b15
toni7891 <toni7891@gmail.com>
2019-12-24 21:12:52 +0200

    hi

--- test.c
+++ test.c
@@ -2,16 +2,24 @@
 
 int main(void)
 {
-    char ch;
-    int i = 0;
-    printf("Input a string:");
-    while ((i <= 3))
-    {
-        ch = getch();
-        putch(ch);
-        i++;
-    }
 
-    printf("%c", ch);
+    char x1 = 0;
+    char x2 = 0;
+    char x3 = 0;
+    char x4 = 0;
+    int final = 0;
+
+    printf("Hello world!\nenter a number: ");
+    x1 = getch();
+    x2 = getch();
+    x3 = getch();
+    x4 = getch();
+
+    x1 *= 1000;
+    x2 *= 100;
+    x3 *= 10;
+
+    final = x1 + x2 + x3 + x4;
+    printf("%d", final);
     return 0;
 }
