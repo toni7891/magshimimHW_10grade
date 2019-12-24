f3b6552d00f3f984a5dba308bcced2257fd248d6
toni7891 <toni7891@gmail.com>
2019-12-25 00:45:41 +0200

    hih

--- test.c
+++ test.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
-#include <curses.h>
+#include <conio.h>
 
 int main(void)
 {
 
-    int x1 = 0;
-    int x2 = 0;
-    int x3 = 0;
-    int x4 = 0;
+    char x1 = 0;
+    char x2 = 0;
+    char x3 = 0;
+    char x4 = 0;
     int final = 0;
 
     printf("Hello world!\nenter a number: ");
-    x1 = getch();
-    x2 = getch();
-    x3 = getch();
-    x4 = getch();
 
+    x1 = getche();
+    x2 = getche();
+    x3 = getche();
+    x4 = getche();
+    /*
+    
+    getch();
+    getch();
+    getch();
+    getch();
+    
+    */
+   /*
     x1 *= 1000;
     x2 *= 100;
     x3 *= 10;
+    x4 *= 1;
+    */
 
-    final = x1 + x2 + x3 + x4;
-    printf("%d", final);
+    //final = x1 + x2 + x3 + x4;
+    printf("\n%c %c %c %c", x1 ,x2, x3,x4);
     return 0;
 }
