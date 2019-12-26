bd3f6b3ce73a9f08383670c4b4408f7377fe92ce
toni7891 <toni7891@gmail.com>
2019-12-26 14:05:38 +0200

    hi

--- projectV2.0.c
+++ projectV2.0.c
@@ -8,9 +8,9 @@
 #define POWER2 100
 #define POWER3 10
 
-int randNumber();
+void randNumber();
 
-int getUserInput(int choice)
+int getInput(int num1, int num2, int num3, int num4);
 
 int trys = 0;
 
@@ -20,40 +20,40 @@ int main(void)
 
     do
     {
-    printf("Welcome to  MAGSHIMIM CODE - BREAKER !!!\n");
-    printf("A secret password was chosen to protect the credit card of Pancratius,");
-    printf("the descendant of Antiochus.");
-    printf("Your mission is to stop Pancratius by revealing his secret password.\n");
-    printf("The rules are as follows:");
-    printf("1. In each round you try to guess the secret password (4 distinct digits)");
-    printf("2. After every guess you'll receive two hints about the password");
-    printf("HITS:   The number of digits in your guess which were exactly right.");
-    printf("MISSES: The number of digits in your guess which belongs to the password but were miss-placed.\n");
-    printf("3. If you'll fail to guess the password after a certain number of rounds");
-    printf("Pancratius will buy all the gifts for Hanukkah!!!\n");
-    printf("Please choose the game level:");
-    printf("1 - Easy (20 rounds)");
-    printf("2 - Moderate (15 rounds)");
-    printf("3 - Hard (10 rounds)");
-    printf("4 - Crazy (random number of rounds 5-25)");
-    scanf("Make a choice: %d", &choice);
+        printf("Welcome to  MAGSHIMIM CODE - BREAKER !!!\n");
+        printf("A secret password was chosen to protect the credit card of Pancratius,");
+        printf("the descendant of Antiochus.");
+        printf("Your mission is to stop Pancratius by revealing his secret password.\n");
+        printf("The rules are as follows:");
+        printf("1. In each round you try to guess the secret password (4 distinct digits)");
+        printf("2. After every guess you'll receive two hints about the password");
+        printf("HITS:   The number of digits in your guess which were exactly right.");
+        printf("MISSES: The number of digits in your guess which belongs to the password but were miss-placed.\n");
+        printf("3. If you'll fail to guess the password after a certain number of rounds");
+        printf("Pancratius will buy all the gifts for Hanukkah!!!\n");
+        printf("Please choose the game level:");
+        printf("1 - Easy (20 rounds)");
+        printf("2 - Moderate (15 rounds)");
+        printf("3 - Hard (10 rounds)");
+        printf("4 - Crazy (random number of rounds 5-25)");
+        scanf("Make a choice: %d", &choice);
     } while (choice != 1 && choice != 2 && choice != 3 && choice != 4 &&);
 
     switch (choice)
     {
     case 1:
-        trys = 20; 
-        getUserInput(trys);
+        trys = 20;
+        randNumber();
         break;
 
     case 2:
         trys = 15;
-        getUserInput(trys);
+
         break;
 
     case 3:
         trys = 10;
-        getUserInput(trys);
+
         break;
 
     case 4:
@@ -64,11 +64,10 @@ int main(void)
     }
 }
 
-
-    return 0;
+return 0;
 }
 
-int randNumber()
+void randNumber()
 {
 
     int num1 = 0;
@@ -83,63 +82,34 @@ int randNumber()
     num2 = (rand() % 6) + 1;
     num3 = (rand() % 6) + 1;
     num4 = (rand() % 6) + 1;
-    
-    finalNum = (num1 * POWER1) + (num2 * POWER2) + (num3 * POWER3) + (num4 * 1);
-    
-    return finalNum;
+
+    getInput(num1, num2, num3, num4);
 }
 
-
-int getInput()
+int getInput(int num1, int num2, int num3, int num4)
 {
     char x1;
     char x2;
     char x3;
     char x4;
-
-    do 
+    int miss = 0;
+    int bull = 0;
+    do
     {
-    printf("Write your guess (only 1-6, no ENTER is needed)\n%d guesses left" , );
-    
-    x1 = getche();
-    x2 = getche();
-    x3 = getche();
-    x4 = getche();
+        printf("Write your guess (only 1-6, no ENTER is needed)\n%d guesses left", trys);
 
-    if (x1 < 1 || x1 > 6 || x2 < 1 || x2 > 6 || x3 < 1 || x3 > 6 || x4 < 1 || x4 > 6)
+        x1 = getche();
+        x2 = getche();
+        x3 = getche();
+        x4 = getche();
+
+        if (x1 < 1 || x1 > 6 || x2 < 1 || x2 > 6 || x3 < 1 || x3 > 6 || x4 < 1 || x4 > 6)
+        {
+            printf("Only 1-6 are allowed, try again!");
+        }
+    } while (x1 < 1 || x1 > 6 || x2 < 1 || x2 > 6 || x3 < 1 || x3 > 6 || x4 < 1 || x4 > 6);
+
+    if (x1 == num2 || x1 == num3 || x1 == num4)
     {
-        printf("Only 1-6 are allowed, try again!");
     }
-    }while (x1 < 1 || x1 > 6 || x2 < 1 || x2 > 6 || x3 < 1 || x3 > 6 || x4 < 1 || x4 > 6); 
-
-    if ()
-    {
-    
-    }
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- test.c
+++ test.c
@@ -4,34 +4,20 @@
 int main(void)
 {
 
-    char x1 = 0;
-    char x2 = 0;
-    char x3 = 0;
-    char x4 = 0;
-    int final = 0;
+    char x1 = 4;
+
+    int y1 = 4;
 
     printf("Hello world!\nenter a number: ");
 
-    x1 = getche();
-    x2 = getche();
-    x3 = getche();
-    x4 = getche();
-    /*
+    if (x1 == y1)
+    {
+        printf("yes");
+    }
     
-    getch();
-    getch();
-    getch();
-    getch();
-    
-    */
-   /*
-    x1 *= 1000;
-    x2 *= 100;
-    x3 *= 10;
-    x4 *= 1;
-    */
-
-    //final = x1 + x2 + x3 + x4;
-    printf("\n%c %c %c %c", x1 ,x2, x3,x4);
+    else
+    {
+        printf("fuck");
+    }
     return 0;
 }
