eda7f7382dec0f1768e37efa518f9c013418df2e
toni7891 <toni7891@gmail.com>
2020-01-10 19:25:40 +0200

    hi

--- hw10/Q6/Q6.c
+++ hw10/Q6/Q6.c
@@ -9,16 +9,27 @@
 
 #define ARR_LENGTH 2
 
-int[] initArray(int arr[]);
+/*
+bug report:
+1. changed "int arr[]" to "int myGrades[]"
+2. changed int[] to int in the declare of function initArray()
+3. gave initArray a pointer
+4. when you want to change the values in array by function just call the function
+5. gave to myGrades an index of length
+6. int first = myGrades; to "int first = myGrades[0];" (index of the first elemnt in array)
+7. changed arr to myGrades in initArray function 
+*/
+
+int * initArray(int myGrades[]);
 void printGrades(int first, int second);
 
 int main(void)
 {
-    int myGrades[] = {0, 0};
+    int myGrades[ARR_LENGTH] = {0,0};
 
-    myGrades = initArray(myGrades);
+    initArray(myGrades);
 
-    int first = myGrades;
+    int first = myGrades[0];
     int second = myGrades[1];
 
     printGrades(first, second);
@@ -28,22 +39,26 @@ int main(void)
 /*
 Function will change values of array (input from user)
 input: array to change
-output: fixed array
+output: array with updated values
 */
-int[] initArray(int arr[])
+
+int * initArray(int myGrades[])
 {
+
     printf("Enter grades in English and history: ");
-    scanf("%d %d", &arr[0], &arr[1]);
-    return arr;
+    scanf("%d %d", &myGrades[0], &myGrades[1]);
+
+    return myGrades;
 }
 
 /*
 Function will print two grades
 input: the grades to print
-output: none
+output: printf the gardes 
 */
+
 void printGrades(int first, int second)
 {
     printf("My grade in English is: %d!\n", first);
-    printf("My grade in History is %d! Great Success!\n", second);
+    printf("My grade in History is: %d!\nGreat Success!\n", second);
 }
