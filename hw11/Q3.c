afeb9879034054fd1ad837578e3dd52ade0012f4
toni7891 <toni7891@gmail.com>
2020-01-17 14:12:12 +0200

    hi

--- hw11/Q3.c
+++ hw11/Q3.c
@@ -5,13 +5,21 @@
 
 int main(void)
 {
-char userInput[MAXINPUT];
 
-printf("Enter string (max length 100 chars)");
-gets(userInput);
-printf("%s" , userInput);
+    char userInput[MAXINPUT];
+    char reversed[MAXINPUT];
+    printf("Enter string (max length 100 chars)");
+    gets(userInput);
+    strcpy(reversed , userInput);
+    if (strcmp(userInput, reversed) == 0)
+    {
+        printf("Yes");
+    }
 
-
-
-return 0;
+    else
+    {
+        printf("No");
+    }
+    
+    return 0;
 }
