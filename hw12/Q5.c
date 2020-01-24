b674dedc39c1ca684bd9bf2f88ce7f2b61b6e572
toni7891 <toni7891@gmail.com>
2020-01-24 16:57:11 +0200

    hi

--- hw12/Q5.c
+++ hw12/Q5.c
@@ -5,29 +5,52 @@
 #define WORDS 10
 #define LETTERS 50
 
+int checkPangram(char str);
+
 int main(void)
 {
     int i = 0;
     int j = 0;
 
-    char input[WORDS][LETTERS];
+    char str[WORDS][LETTERS];
 
     for (i = 0; i < WORDS; i++)
     {
-            scanf("%s", input[i]);
+        scanf("%s", str[i]);
     }
-
-    // check panagram algo
-    for (i = 0; i < WORDS; i++)
+    printf("It's pangram?");
+    if (checkPangram(str) == 26)
     {
-        for (j = 0; j < LETTERS; j++)
-        {
-            /* code */
-        }
-        
+        printf("Yes");
     }
-    
-    
 
+    else if (checkPangram(str) != 26)
+    {
+        printf("No");
+    }
     return 0;
+}
+
+int checkPangram(char str)
+{
+    int i = 0;
+    int j = 0;
+    int used[26] = {0}; //to mark every letter in alphabet [english alphabet contains 26 letters]
+    int total = 0;
+
+    //reference: https://www.codingconnect.net/c-program-check-given-string-pangram/
+    //
+    for (j = 0; str[j] < WORDS; j++)
+    {
+        for (i = 0; str[j][i] != '\0'; i++)
+        {
+            if ('a' <= str[j][i] && str[j][i] <= 'z')
+            {
+                total = total + !used[str[i] - 'a'];
+                used[str[i] - 'a'] = 1;
+            }
+        }
+    }
+
+    return total;
 }
