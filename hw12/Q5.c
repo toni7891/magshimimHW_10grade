c9bb6bf4b4c11a9700bf291e6645b07edcf3b480
toni7891 <toni7891@gmail.com>
2020-01-25 17:37:50 +0200

    hi

--- hw12/Q5.c
+++ hw12/Q5.c
@@ -9,11 +9,37 @@ int main(void)
 {
     int i = 0;
     int j = 0;
-    int used[26] = {0}; //to mark every letter in alphabet [english alphabet contains 26 letters]
     int total = 0;
 
     char str[WORDS][LETTERS];
-    char letter[26] = {'a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',};
+    char letter[26] = {
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'g',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+    };
 
     printf("Enter up to 10 words, try to make pangram:\n");
     for (i = 0; i < WORDS; i++)
@@ -22,13 +48,30 @@ int main(void)
     }
     printf("It's pangram?");
 
-    for (i = 0; str[i][] < 2; i++)
+    for (i = 0; str[i][j] < WORDS; i++)
     {
-        for (j = 0; str[i][j] < 2; j++)
+        for (j = 0; str[i][j] < LETTERS; j++)
         {
-            
+            if (total != 26)
+            {
+                if (str[i][j] == letter[j])
+                {
+                    total = total + 1;
+                }
+            }
         }
     }
+
+    if (total = 26)
+    {
+        printf("Yes");
+    }
+
+    else
+    {
+        printf("No");
+    }
+    
 
     return 0;
 }
