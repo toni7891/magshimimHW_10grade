4b41634e796a9bbffbd36392049e60cbaae7ce4a
toni7891 <toni7891@gmail.com>
2020-01-31 12:30:34 +0200

    hi

---
+++ hw13/Q4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+#define ARR_LENGTH 
+
+void countingSort(int arr[]);
+
+int main(void)
+{
+    int arr[ARR_LENGTH] = {0};
+    int i = 0;
+
+    printf("Enter 17 numbers: ");
+    for (i = 0; i < ARR_LENGTH; ++i)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    void countingSort(arr[]);
+
+    return 0;
+}
+
+void countingSort(int arr[])
+{
+    int i = 0;
+    
+    for (i = 0; i < ARR_LENGTH; i++)
+    {
+        if ()
+        {
+        
+        }        
+    }
+
+
+
+}
