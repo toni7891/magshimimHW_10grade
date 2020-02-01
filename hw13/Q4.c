67f1b9a3690518dab45e98663f4b61670661ae3c
toni7891 <toni7891@gmail.com>
2020-02-01 12:36:49 +0200

    hi

--- hw13/Q4.c
+++ hw13/Q4.c
@@ -1,38 +1,58 @@
 #include <stdio.h>
-#include <string.h>
 
-#define ARR_LENGTH 
+#define MAX_RANGE 256
+#define ARR_LENGTH 17
 
-void countingSort(int arr[]);
-
-int main(void)
+int main()
 {
-    int arr[ARR_LENGTH] = {0};
-    int i = 0;
+    int array[ARR_LENGTH] = {35, 123, 7, 9, 4, 2, 2, 8, 3, 3, 1, 23, 56, 54, 12, 89, 54};
 
-    printf("Enter 17 numbers: ");
-    for (i = 0; i < ARR_LENGTH; ++i)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    void countingSort(arr[]);
-
-    return 0;
+    countingSort(array);
+    printArray(array);
 }
 
-void countingSort(int arr[])
+
+
+void countingSort(int array[])
 {
+    int output[17];
+    int max = array[0];
+    int count[17];
     int i = 0;
-    
-    for (i = 0; i < ARR_LENGTH; i++)
+
+    for (i = 1; i < ARR_LENGTH; i++)
     {
-        if ()
+        if (array[i] > max)
         {
-        
-        }        
+            max = array[i];
+        }
     }
 
+    for (i = 0; i <= max; ++i)
+    {
+        count[i] = 0;
+    }
 
+    for (i = 0; i < ARR_LENGTH; i++)
+    {
+        count[array[i]]++;
+    }
 
+    for (i = 1; i <= max; i++)
+    {
+        count[i] += count[i - 1];
+    }
+
+    for (i = ARR_LENGTH - 1; i >= 0; i--)
+    {
+        output[count[array[i]] - 1] = array[i];
+        count[array[i]]--;
+    }
+
+    for (i = 0; i < ARR_LENGTH; i++)
+    {
+        array[i] = output[i];
+    }
+
+    
 }
