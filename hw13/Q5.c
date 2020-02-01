eaca499227834f3163b81e5c21113102512764ba
toni7891 <toni7891@gmail.com>
2020-02-01 23:08:17 +0200

    hi

--- hw13/Q5.c
+++ hw13/Q5.c
@@ -2,16 +2,18 @@
 
 int main(void)
 {
+
+    int i = 0;
     char s1[25];
     char s2[25];
 
     printf("input first string: ");
-    //fgets(s1, 25, stdin);
-    scanf("%[^\n]%*c", s1);
+    fgets(s1, 25, stdin);
+    s1[strlen(s1) - 1] = '\0';
 
     printf("input second string: ");
-    //fgets(s2, 25, stdin);
-    scanf("%[^\n]%*c", s2);
+    fgets(s2, 25, stdin);
+    s2[strlen(s2) - 1] = '\0';
 
     printf("First string length: %d \n", strlen(s1));
     printf("Second string length: %d \n", strlen(s2));
