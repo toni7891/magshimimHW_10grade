d5380cbe87ac081a1bcc3e8d0f89eece29d9fdf3
toni7891 <toni7891@gmail.com>
2020-03-13 03:09:48 +0200

    hi

--- python course/python_questions.py
+++ python course/python_questions.py
@@ -1,11 +1,9 @@
 from collections import OrderedDict
 
-
 def main():
   user_input_str = input("Enter an shop list[item,item,item]: ")
   user_input_str = user_input_str.split(",")
   menu(user_input_str)
-
 
 def menu(user_list):
   """
@@ -14,7 +12,7 @@ def menu(user_list):
   :return: None
   """
   user_choose = 0
-  while not user_choose == 9:
+  while user_choose != 9:
     user_choose = int(input("""\t1: Print the list.
     2: Print the number of item's in the list.
     3: Check if item in list.
@@ -48,11 +46,13 @@ def menu(user_list):
       item_to_add = input("Enter the item to add to the list: ")
       add_item_to(item_to_add, user_list)
 
+
     elif user_choose == 7:
       not_alpha_chars(user_list)
 
     elif user_choose == 8:
-      dupli_remover(user_list)
+      user_list = list(dict.fromkeys(user_list))
+      print("New list without duplicate's: ", user_list)
 
     elif user_choose == 9:
       print("Good bye!")
@@ -68,7 +68,6 @@ def print_list(user_list):
   print(user_list)
   return user_list
 
-
 def print_num_items(user_list):
   """
   Prints number of item's in the list.
@@ -77,32 +76,58 @@ def print_num_items(user_list):
   """
   print(len(user_list))
 
-
 def check_item_in(item, user_list):
+  """
+  Check if item exist in list.
+  :param item: Item to search in list.
+  :param user_list: list to search in.
+  :return: False / True
+  """
   if item in user_list:
+    print(True)
     print("The searched item: ", item, ", exist in the list!")
+    return True
   else:
+    print(False)
     print("The item: ", item, ", dosen't exist in the list!")
-
-
+    return False
 def how_many_apperience(item, user_list):
+  """
+  Check how many times Item appear in the list.
+  :param item: Item to count appearance.
+  :param user_list: List to count appearance in.
+  :return: None
+  """
   how_many = user_list.count(item)
   print("The item: ", item, "apper's in the list", how_many, "time's!")
 
-
 def remove_item_from(item, user_list):
+  """
+  Remove item from the list.
+  :param item: Item to remove.
+  :param user_list: List to remove from.
+  :return: None
+  """
   user_list.remove(item)
   print("Item removed!")
-  print("the list after the remove of ", item, ":", user_list)
-
+  print("The list after the remove of ",item, ":", user_list)
 
 def add_item_to(item, user_list):
-
+  """
+  Add item to the list.
+  :param item: Item to add
+  :param user_list: List to add item to.
+  :return: None
+  """
   user_list.append(item)
-  print("the list after adding the item: ", user_list)
-
+  print("The list after adding the item: ", user_list)
 
 def not_alpha_chars(user_list):
+  """
+  Check if the item's are valid.
+  :param user_list: List to check validation of item's in.
+  :return: None
+  """
   new_list = []
   flag = 0
   for item in user_list:
@@ -116,11 +141,6 @@ def not_alpha_chars(user_list):
   elif flag == 0:
     print("All of the item's are valid!")
 
-
-def dupli_remover(user_list):
-  user_list = list(dict.fromkeys(user_list))
-  print(user_list)
-
-
 if __name__ == "__main__":
   main()
+
---
+++ semester 2/HW1/Q3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 51
+#define LETTERS_LEN 28
+
+int main(void)
+{
+    int x = 0;
+    int z = 0;
+    char str[MAX_LEN];
+    char lower[LETTERS_LEN] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char upper[LETTERS_LEN] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char upperStr[MAX_LEN];
+    char lowerStr[MAX_LEN];
+    
+    printf("Enter a string with upper and lower case letters: ");
+    fgets(str, MAX_LEN, stdin);
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        for (int j = 0; j < LETTERS_LEN; j++)
+        {
+            if (str[i] == lower[j])
+            {
+                upperStr[x] = str[i];
+                x++;
+            }
+        }
+
+        for (int n = 0; n < LETTERS_LEN; n++)
+        {
+            if (str[i] == upper[n])
+            {
+                lowerStr[z] = str[i]; 
+                z++;
+            }
+        }
+    }
+
+    lowerStr[strlen(lowerStr) + 1] = 0;
+    upperStr[strlen(upperStr) + 1] = 0;
+
+    printf("The upper and lower case words:\n%s\n%s", lowerStr, upperStr);
+    getchar();
+    return 0;
+}
